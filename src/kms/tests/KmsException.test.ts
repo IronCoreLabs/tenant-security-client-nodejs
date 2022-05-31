1ebecb956d7a26c63ba55259fbc8299ea9dd20a4
Colt Frederickson <coltfred@gmail.com>
2022-05-31 13:38:43 -0600

    Understand error 209

--- src/kms/tests/KmsException.test.ts
+++ src/kms/tests/KmsException.test.ts
@@ -11,4 +11,11 @@ describe("UNIT KmsException", () => {
     test("construction fails when an invalid code is provided", () => {
         expect(() => new KmsException(TenantSecurityErrorCode.DOCUMENT_DECRYPT_FAILED, "error message", 0)).toThrow();
     });
+
+    test("Understands error 209", () => {
+        const error = new KmsException(209, "totally real error message.", 429);
+        expect(error.errorCode).toEqual(TenantSecurityErrorCode.KMS_THROTTLED);
+        expect(error.httpResponseCode).toEqual(429);
+        expect(error.message).toEqual("totally real error message.");
+    });
 });
