34bc1156898ce7fa756b993426af623d4bb2a799
Murph Murphy <murph@clurictec.com>
2022-11-09 11:26:09 -0700

    Add a basic nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "tenant-security-client-nodejs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        lib = import <nixpkgs/lib>;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = {
+          tenant-security-client-nodejs = {
+            name = "tenant-security-client-nodejs";
+            version = "0.1.0";
+            src = ./.;
+            buildInputs = with pkgs.nodePackages; [
+              pkgs.nodejs-16_x
+              pkgs.protobuf
+              (pkgs.yarn.override { nodejs = nodejs-16_x; })
+            ];
+            nativeBuildInputs = [ ]
+              ++ lib.optionals (pkgs.stdenv.isDarwin) [ pkgs.darwin.cctools ];
+          };
+        };
+        defaultPackage = packages.tenant-security-client-nodejs;
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs.nodePackages; [
+            pkgs.nodejs-16_x
+            pkgs.protobuf
+            (pkgs.yarn.override { nodejs = pkgs.nodejs-16_x; })
+          ];
+        };
+      });
+}
