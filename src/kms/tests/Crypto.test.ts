2c0dda2719a4a4afff5c54de026bc23786e6e733
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2023-06-15 13:03:44 -0700

    Add some more tests

--- src/kms/tests/Crypto.test.ts
+++ src/kms/tests/Crypto.test.ts
@@ -10,7 +10,7 @@ const derivedKeys = [
         primary: true,
     },
     {
-        derivedKey: "XEECPt9kGfeATh95LuV3k+UO63CXxLnk0dPXgGK6FVY=", // TODO: different valid key
+        derivedKey: "drd++bJKeHJW04sLiz3S0YOLI+oZUyYccIrMvZwPDPw=",
         tenantSecretId: "bbb",
         tenantSecretNumericId: 6,
         primary: false,
@@ -43,14 +43,16 @@ describe("UNIT deterministicEncryptField", () => {
     });
 });
 
-describe("UNIT deterministicSearch", () => {
-    test("returns 2 entries for 2 keys", async () => {
-        const encrypted = await Crypto.deterministicSearch(plaintextField, derivedKeys);
-        expect(encrypted.length).toBe(2);
-        const result1FirstBytes = encrypted[0].encryptedField.slice(0, 4);
-        const result2FirstBytes = encrypted[1].encryptedField.slice(0, 4);
-        // the keys have different IDs encoded in the first 4 bytes
-        expect(Buffer.compare(result1FirstBytes, result2FirstBytes)).not.toBe(0);
+describe("UNIT generateDeterministicEncryptedFieldHeader", () => {
+    test("works for valid tenantSecretId", async () => {
+        const header = await Crypto.generateDeterministicEncryptedFieldHeader(4);
+        const expected = Buffer.from([0, 0, 0, 4, 0, 0]);
+        expect(Buffer.compare(header, expected)).toBe(0);
+    });
+
+    test("fails for invalid tenantSecretId", async () => {
+        await expect(Crypto.generateDeterministicEncryptedFieldHeader(-1)).rejects.toThrow("Failed to generate header");
+        await expect(Crypto.generateDeterministicEncryptedFieldHeader(9999999999)).rejects.toThrow("Failed to generate header");
     });
 });
 
@@ -82,5 +84,31 @@ describe("UNIT generateDeterministicEncryptedFieldHeader", () => {
     test("fails for out of bounds", async () => {
         const header = Crypto.generateDeterministicEncryptedFieldHeader(4294967296);
         await expect(header).rejects.toThrow("generate header");
+    });
+});
+
+describe("UNIT decomposeDeterministicField", () => {
+    test("works for valid header/field", async () => {
+        const encrypted = Buffer.from([0, 0, 0, 4, 0, 0, 1, 1, 1]);
+        const parts = await Crypto.decomposeDeterministicField(encrypted);
+        expect(parts.tenantSecretId).toBe(4);
+        const expectedEncryptedBytes = Buffer.from([1, 1, 1]);
+        expect(Buffer.compare(parts.encryptedBytes, expectedEncryptedBytes)).toBe(0);
+    });
+
+    test("fails for incorrect padding bytes", async () => {
+        const encrypted = Buffer.from([0, 0, 0, 4, 1, 0, 1, 1, 1]);
+        await expect(Crypto.decomposeDeterministicField(encrypted)).rejects.toThrow("Failed to parse");
+    });
+});
+
+describe("UNIT deterministicSearch", () => {
+    test("returns 2 entries for 2 keys", async () => {
+        const encrypted = await Crypto.deterministicSearch(plaintextField, derivedKeys);
+        expect(encrypted.length).toBe(2);
+        const result1FirstBytes = encrypted[0].encryptedField.slice(0, 4);
+        const result2FirstBytes = encrypted[1].encryptedField.slice(0, 4);
+        // the keys have different IDs encoded in the first 4 bytes
+        expect(Buffer.compare(result1FirstBytes, result2FirstBytes)).not.toBe(0);
     });
 });
